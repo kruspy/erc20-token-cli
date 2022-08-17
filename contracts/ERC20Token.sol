85e6a83296d4b876b20a71cb93711fe1efb3e409
Marc Puig <krusspy@gmail.com>
2022-08-17 10:24:29 +0200

    Set token decimals to 6

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -8,4 +8,12 @@ contract ERC20Token is ERC20 {
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
     }
+
+    function decimals() override public virtual view returns (uint8) {
+        // Decimals is overriden to 9 as the Go bindings generated
+        // only support passing the initial supply of the contract as
+        // int64 which can only hold up to 9*10^18. This would effectevly 
+        // leave he whole supply with just 9 full tokens.
+        return 6;
+    }
 }
